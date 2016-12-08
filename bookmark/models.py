44a4e010c20a5ae1d7afada639b8321b71d8a9b6
Chinseok Lee <allieuslee@gmail.com>
2016-12-08 22:43:35 +0900

    Bookmark 모델을 만들고, 마이그레이션 수행

--- bookmark/models.py
+++ bookmark/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Bookmark(models.Model):
+    title = models.CharField(max_length=100, blank=True)
+    url = models.URLField(unique=True)
+
+    def __str__(self):
+        return self.title
+
