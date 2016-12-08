0d492c8243e2772fac9ee90ce6b7320d3ca9ce1e
Chinseok Lee <allieuslee@gmail.com>
2016-12-08 23:03:50 +0900

    bookmark list 뷰를 FBV로 구현

--- bookmark/views.py
+++ bookmark/views.py
@@ -3,7 +3,15 @@ from django.views.generic import ListView
 from .models import Bookmark
 
 
+'''
 class BookmarkLV(ListView):
     model = Bookmark
 
 bookmark_list = BookmarkLV.as_view()
+'''
+
+def bookmark_list(request):
+    return render(request, 'bookmark/bookmark_list.html', {
+        'bookmark_list': Bookmark.objects.all(),
+    })
+
