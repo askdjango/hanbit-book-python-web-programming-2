334bbadecf05cacb1d15c084a1c3bb3ec9de7859
Chinseok Lee <allieuslee@gmail.com>
2016-12-08 22:52:06 +0900

    커스텀 Bookmark Admin 등록

--- bookmark/admin.py
+++ bookmark/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 from .models import Bookmark
 
 
-admin.site.register(Bookmark)
+@admin.register(Bookmark)
+class BookmarkAdmin(admin.ModelAdmin):
+    list_display = ['title', 'linked_url']
+
+    def linked_url(self, bookmark):
+        return '<a href="{url}" target="_blank">{url}</a>'.format(url=bookmark.url)
+    linked_url.allow_tags = True
+
+
+# admin.site.register(Bookmark, BookmarkAdmin)
 
