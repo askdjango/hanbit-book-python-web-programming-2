b7f2e4d51a9f747dacd7767b4d3fedd95e725552
Chinseok Lee <allieuslee@gmail.com>
2016-12-08 23:17:45 +0900

    blog.models.Post 모델을 생성/마이그레이션, admin 에 등록

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(unique=True, allow_unicode=True)
+    description = models.CharField(max_length=100, blank=True)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, db_index=True)
+
+    class Meta:
+        ordering = ['-updated_at']
+
+    def __str__(self):
+        return self.title
+
