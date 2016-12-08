264bb17edc7c89a5c42a9eff84889b2d08cdde00
Chinseok Lee <allieuslee@gmail.com>
2016-12-08 22:34:45 +0900

    settings 내 templates/static/media 설정 추가

--- askdjango/settings.py
+++ askdjango/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'askdjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'askdjango', 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,4 +120,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/'  # url prefix
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'askdjango', 'static'),
+]
+
+MEDIA_URL = '/media/'  # url prefix
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
