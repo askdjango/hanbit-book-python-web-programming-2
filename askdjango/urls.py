290cc5c204e562537ed911375887296ea554f3db
Chinseok Lee <allieuslee@gmail.com>
2016-12-15 22:42:40 +0900

    각 앱의 urls 에 namespace 지정

--- askdjango/urls.py
+++ askdjango/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^bookmark/', include('bookmark.urls')),
-    url(r'^blog/', include('blog.urls')),
+    url(r'^bookmark/', include('bookmark.urls', namespace='bookmark')),
+    url(r'^blog/', include('blog.urls', namespace='blog')),
 ]
