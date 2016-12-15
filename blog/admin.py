36cbca7bf7057ecfea3199b1fbcd52a6b2f18cca
Chinseok Lee <allieuslee@gmail.com>
2016-12-15 22:33:18 +0900

    blog/PostAdmin 커스텀 : list_display, list_filter, search_fields, prepopulated_fields

--- blog/admin.py
+++ blog/admin.py
@@ -3,9 +3,15 @@ from .models import Post
 
 
 class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'content_length', 'updated_at']
+    list_filter = ['updated_at']
+    search_fields = ['title']
     prepopulated_fields = {
         'slug': ['title'],
     }
+
+    def content_length(self, post):
+        return '{}글자'.format(len(post.content))
 
 
 admin.site.register(Post, PostAdmin)
