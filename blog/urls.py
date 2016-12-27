eb49d8aee73c6430a0d446719a9d67153b5e34bc
Chinseok Lee <allieuslee@gmail.com>
2016-12-27 12:29:10 +0900

    blog.views.PostLV 뷰 구현

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url
+from blog.views import PostLV
 
 urlpatterns = [
+    url(r'^$', PostLV.as_view(), name='index'),  # "blog:index"
+    url(r'^post/$', PostLV.as_view(), name='post_list'),
 ]
 
