f5e96f5d9abc06ef4fb699f4627ab0006aa06f04
Chinseok Lee <allieuslee@gmail.com>
2016-12-27 12:36:08 +0900

    blog.views.PostDV 뷰 구현

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from blog.views import PostLV
+from blog.views import PostLV, PostDV
 
 urlpatterns = [
     url(r'^$', PostLV.as_view(), name='index'),  # "blog:index"
     url(r'^post/$', PostLV.as_view(), name='post_list'),
+    url(r'^post/(?P<slug>[-\w]+)/$', PostDV.as_view(), name='post_detail'),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from blog.models import Post
 
 
 class PostLV(ListView):
     model = Post
     paginate_by = 4
+
+
+class PostDV(DetailView):
+    model = Post
 
